c42cb57023e7f9e9f323fccc6547af1681e37cfe
sharvanich <130927204+sharvanich@users.noreply.github.com>
2024-11-23 16:22:30 +0530

    Update syscall.c

--- System Call Enhancements in xv6 OS/kernel/syscall.c
+++ System Call Enhancements in xv6 OS/kernel/syscall.c
@@ -102,6 +102,7 @@ extern uint64 sys_unlink(void);
 extern uint64 sys_link(void);
 extern uint64 sys_mkdir(void);
 extern uint64 sys_close(void);
+extern unit64 sys_setpriority(void);
 
 // An array mapping syscall numbers from syscall.h
 // to the function that handles the system call.
